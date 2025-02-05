9a15af52a0b9ac35e1bb1edb386468e78ab703b0
zinongli <131403964+zinongli@users.noreply.github.com>
2025-02-05 02:00:07 -0500

    debug memory leak. read after load still bug

--- scenes/metroflip_scene_parse.c
+++ scenes/metroflip_scene_parse.c
@@ -53,12 +53,12 @@ bool metroflip_scene_parse_on_event(void* context, SceneManagerEvent event) {
 
 void metroflip_scene_parse_on_exit(void* context) {
     Metroflip* app = context;
-
-    if(!((app->card_type[0] == '\0') || (strcmp(app->card_type, "unknown") == 0) ||
-         (!app->card_type))) {
+    FURI_LOG_I(TAG, "Card type = %s", app->card_type);
+    if(!((app->card_type[0] == '\0') || (strcmp(app->card_type, "unknown") == 0) || (!app->card_type))) {
         // Get and run the plugin's on_exit function
         const MetroflipPlugin* plugin = plugin_manager_get_ep(app->plugin_manager, 0);
         plugin->plugin_on_exit(app);
+        FURI_LOG_I(TAG, "Parse scene exit");
 
         plugin_manager_free(app->plugin_manager);
         composite_api_resolver_free(app->resolver);
