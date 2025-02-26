afbd16cb7a915de9a96172afbc969fe1eee811d6
zinongli <131403964+zinongli@users.noreply.github.com>
2025-02-26 13:48:43 -0500

    fix include bug

--- api/suica/suica_drawings.h
+++ api/suica/suica_drawings.h
@@ -7,7 +7,6 @@
 
 #include <lib/nfc/protocols/felica/felica.h>
 #include <lib/nfc/protocols/felica/felica_poller.h>
-#include <lib/nfc/protocols/felica/felica_poller_i.h>
 #include <lib/nfc/helpers/felica_crc.h>
 #include <lib/bit_lib/bit_lib.h>
 
